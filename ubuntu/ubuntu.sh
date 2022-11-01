2ef6fb6aa978361b6b372f86b2901d9c54afea6d
Felipe Soares <felipe.soares013@outlook.com>
2022-11-01 15:07:58 -0300

    criado pasta ubuntu

---
+++ ubuntu/ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+# Peek (screen recorder)
+$ sudo add-apt-repository ppa:peek-developers/stable
+$ sudo apt-get update
+$ sudo apt-get install peek
+
+# gimp
+$ sudo apt-get install gimp
